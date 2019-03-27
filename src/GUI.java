15db7bc87974f7536764abe294691415e16d6593
Danny Toeun Kim <34687639+kimdanny@users.noreply.github.com>
2019-03-27 16:53:31 +0000

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
@@ -8,6 +7,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * <Create a simple dashboard application>
@@ -27,9 +27,10 @@ import java.io.IOException;
  *
  */
 
+
 public class GUI extends JFrame {
 
-    Model model = new Model();
+    Model model;
 
     // Panels
         // Back Panel
@@ -38,11 +39,13 @@ public class GUI extends JFrame {
     private JPanel listBackPanel;
     private JPanel textBackPanel;
         // search sub-panels
-    private JPanel searchAreaPanel;
+    private JPanel searchByIDAreaPanel;
+    private JPanel searchByNameAreaPanel;
     private JPanel searchButtonPanel;
+
         // list sub-panels
-    //private JPanel listScrollerPanel;
     private JPanel listButtonPanel;
+    private JPanel listStatButtonPanel;
         // text sub-panels
     private JPanel textAreaPanel;
     private JPanel textButtonPanel;
@@ -54,6 +57,8 @@ public class GUI extends JFrame {
         // In listButtonPanel
     private JButton averageAgeButton;
     private JButton mostCommonYearButton;
+    private JButton removeButton;
+    private JButton showSinglePatientButton;
         // In textButtonPanel
     private JButton cutButton;
     private JButton copyButton;
@@ -62,7 +67,9 @@ public class GUI extends JFrame {
     private JButton saveButton;
 
     // Regarding Search
-    private JTextArea searchArea;
+    // JTextField is one row text area -> good for search area
+    private JTextField searchByIDArea;
+    private JTextField searchByNameArea;
 
     // Regarding List
     private JList<String> list;
@@ -75,28 +82,38 @@ public class GUI extends JFrame {
 
     // constructor - main part of GUI
     public GUI() {
-        super("Patient GUI");
+        // initial Settings
+        super("Patients GUI");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
-        setBounds(200, 100, 1000, 700);
-        // initial settings for constructor
-        // here put functions below
+        // Set size to ScreenSize
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = (int) screenSize.getHeight();
+        int screenWidth = (int) screenSize.getWidth();
+        setBounds(0, 0, screenWidth, screenHeight);
+
         createGUI();
         setList();
 
-
         setVisible(true);
+
+        model = new Model(loadFile());
     }
 
+
+
     public void createGUI(){
-        //setTitle("Patient GUI with set title");
         createListScrollerPanel();
         createListButtonPanel();
+        createStatisticsListButtonPanel();
         createListBackPanel();
+
         createTextAreaPanel();
         createTextButtonPanel();
         createTextBackPanel();
-        createSearchAreaPanel();
+
+        createSearchByIDAreaPanel();
+        createSearchByNameAreaPanel();
         createSearchButtonPanel();
         createSearchBackPanel();
 
@@ -117,14 +134,20 @@ public class GUI extends JFrame {
      * Search area (North)*
      *********************/
 
-    private void createSearchAreaPanel(){
-        searchArea = new JTextArea();
-        searchArea.setColumns(30);
-        searchArea.setRows(1);
-        searchArea.setText("Search here");
-        searchAreaPanel = new JPanel(new BorderLayout());
-        searchAreaPanel.setBorder(BorderFactory.createEtchedBorder());
-        searchAreaPanel.add(searchArea, BorderLayout.CENTER);
+    private void createSearchByIDAreaPanel(){
+        searchByIDArea = new JTextField();
+        searchByIDArea.setColumns(35);
+        searchByIDAreaPanel = new JPanel(new BorderLayout());
+        searchByIDAreaPanel.setBorder(BorderFactory.createTitledBorder("Search by ID"));
+        searchByIDAreaPanel.add(searchByIDArea, BorderLayout.CENTER);
+    }
+
+    private void createSearchByNameAreaPanel(){
+        searchByNameArea = new JTextField();
+        searchByNameArea.setColumns(15);
+        searchByNameAreaPanel = new JPanel(new BorderLayout());
+        searchByNameAreaPanel.setBorder(BorderFactory.createTitledBorder("Search by First Name"));
+        searchByNameAreaPanel.add(searchByNameArea, BorderLayout.CENTER);
     }
 
     private void createSearchButtonPanel(){
@@ -135,11 +158,13 @@ public class GUI extends JFrame {
         searchButtonPanel.add(loadAllPatientsButton);
     }
 
+
     private void createSearchBackPanel(){
         searchBackPanel = new JPanel(new BorderLayout());
         searchBackPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
-        searchBackPanel.add(searchAreaPanel, BorderLayout.WEST);
-        searchBackPanel.add(searchButtonPanel, BorderLayout.CENTER);
+        searchBackPanel.add(searchByIDAreaPanel, BorderLayout.WEST);
+        searchBackPanel.add(searchByNameAreaPanel, BorderLayout.CENTER);
+        searchBackPanel.add(searchButtonPanel, BorderLayout.EAST);
     }
 
     /*********************
@@ -147,12 +172,12 @@ public class GUI extends JFrame {
      *********************/
 
     private void setList() {
-        listModel = new DefaultListModel<String>();
+        listModel = new DefaultListModel<>();
         list.setModel(listModel);
     }
 
     private void createListScrollerPanel(){
-        list = new JList<String>();
+        list = new JList<>();
         list.addMouseListener(
                 new MouseAdapter()
                 {
@@ -167,10 +192,18 @@ public class GUI extends JFrame {
 
     private void createListButtonPanel(){
         listButtonPanel = new JPanel();
+        createRemoveButton();
+        createShowSinglePatientButton();
+        listButtonPanel.add(removeButton, 0);
+        listButtonPanel.add(showSinglePatientButton, 1);
+    }
+
+    private void createStatisticsListButtonPanel(){
+        listStatButtonPanel = new JPanel();
         createAverageAgeButton();
         createMostCommonYearButton();
-        listButtonPanel.add(averageAgeButton);
-        listButtonPanel.add(mostCommonYearButton);
+        listStatButtonPanel.add(averageAgeButton, 0);
+        listStatButtonPanel.add(mostCommonYearButton, 1);
     }
 
     private void createListBackPanel(){
@@ -178,25 +211,25 @@ public class GUI extends JFrame {
         listBackPanel.setBorder(BorderFactory.createTitledBorder("Searched result"));
         listBackPanel.add(listScroller, BorderLayout.NORTH);
         listBackPanel.add(listButtonPanel, BorderLayout.CENTER);
+        listBackPanel.add(listStatButtonPanel, BorderLayout.SOUTH);
     }
 
 
     /*******************
-     * text area (East)*
+     * Text area (East)*
      *******************/
     private void createTextAreaPanel(){
         textArea = new JTextArea();
         textArea.setColumns(40);
-        textArea.setText("Load files or type here");
+        //textArea.setText("Load files or type here");
         textScroller = new JScrollPane(textArea);
         textAreaPanel = new JPanel(new BorderLayout());
-        //textAreaPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         textAreaPanel.add(textScroller, BorderLayout.CENTER);
 
     }
 
     private void createTextButtonPanel(){
-        textButtonPanel = new JPanel(/*new GridLayout(1, 1, 1, 1)*/);
+        textButtonPanel = new JPanel();
         createLoadButton();
         createSaveButton();
         createCutButton();
@@ -223,27 +256,27 @@ public class GUI extends JFrame {
      * These methods should be included in create'X'ButtonPanel()
      */
     private void createLoadButton() {
-        loadButton = new JButton("Load");
+        loadButton = new JButton("Load CSV");
         loadButton.addActionListener((ActionEvent e) -> loadFile());
     }
 
     private void createSaveButton() {
-        saveButton = new JButton("Save");
+        saveButton = new JButton("Save as JSON");
         saveButton.addActionListener((ActionEvent e) -> saveFile());
     }
 
     private void createCutButton() {
-        cutButton = new JButton("Cut");
+        cutButton = new JButton("Cut text");
         cutButton.addActionListener((ActionEvent e) -> cut());
     }
 
     private void createCopyButton() {
-        copyButton = new JButton("Copy");
+        copyButton = new JButton("Copy text");
         copyButton.addActionListener((ActionEvent e) -> copy());
     }
 
     private void createPasteButton() {
-        pasteButton = new JButton("Paste");
+        pasteButton = new JButton("Paste text");
         pasteButton.addActionListener((ActionEvent e) -> paste());
     }
 
@@ -267,13 +300,23 @@ public class GUI extends JFrame {
         loadAllPatientsButton.addActionListener((ActionEvent e) -> loadAllPatientsButtonClicked());
     }
 
+    private void createRemoveButton(){
+        removeButton = new JButton("Remove");
+        removeButton.addActionListener((ActionEvent e) -> removeButtonClicked());
+    }
+
+    private void createShowSinglePatientButton(){
+        showSinglePatientButton = new JButton("Show the Patient Information");
+        showSinglePatientButton.addActionListener((ActionEvent e) -> showSinglePatientButtonClicked());
+    }
+
 
 
     /**
      * Controller
      * Below methods are responses for the action
      */
-    private void loadFile()
+    private String loadFile()
     {
         JFileChooser fc = new JFileChooser();
         int returnVal = fc.showOpenDialog(this);
@@ -282,15 +325,18 @@ public class GUI extends JFrame {
             File file = fc.getSelectedFile();
             try
             {
-                textArea.read(new FileReader(file), null);
+                String filepath = file.getAbsolutePath();
+                return filepath;
             }
-            catch (IOException exp)
+            catch (Exception exp)
             {
                 JOptionPane.showMessageDialog(this, "Unable to load the file", "File Error",
                         JOptionPane.ERROR_MESSAGE);
             }
         }
+        return null;
     }
+
 
     private void saveFile()
     {
@@ -311,65 +357,127 @@ public class GUI extends JFrame {
         }
     }
 
-    private void cut()
-    {
+    private void cut() {
         textArea.cut();
         textArea.requestFocus();
     }
 
-    private void copy()
-    {
+    private void copy() {
         textArea.copy();
         textArea.requestFocus();
     }
 
-    private void paste()
-    {
+    private void paste() {
         textArea.paste();
         textArea.requestFocus();
     }
 
     private void searchButtonClicked(){
-        /*
-        * 1. search inputfield 에서 getText()를 해서 어떠한 변수(s)에 담아놓는다
-        * 2. if s.length() > 0 :
-        *   2-1. enhanced for loop을 이용하여 서치결과와 맞는 것들을 하나씩 리스트에 추가한다
-        * 3. search inputfield.setText("");
-        * */
+
+        String inputID = searchByIDArea.getText();
+        String inputFirst = searchByNameArea.getText();
+
+        try {
+            // ID is distinct value so just add one element to the listModel
+            if (inputID.length() > 0) {
+                listModel.clear();
+                listModel.addElement(model.searchByID(inputID).getID());
+                searchByIDArea.setText("");
+
+            }
+            // Name is not a distinct value so add inside the for loop (patientList can hold multiple names)
+            if (inputFirst.length() > 0) {
+                List<Patient> patientList = model.searchByFirst(inputFirst);
+                listModel.clear();
+                for (Patient patient : patientList) {
+                    listModel.addElement(patient.getFIRST() + " " + patient.getLAST());
+                }
+                searchByNameArea.setText("");
+            }
+
+        }
+        catch (NullPointerException e){
+            // when nothing found, popup
+            JOptionPane.showMessageDialog(this, "No Result Found", "Error Message",
+                    JOptionPane.ERROR_MESSAGE);
+            searchByIDArea.setText("");
+            searchByNameArea.setText("");
+        }
 
     }
 
     private void loadAllPatientsButtonClicked(){
-        /*
-         * 1. enhanced for loop을 이용하여 all patients를 하나씩 리스트에 추가한다
-         * */
 
+        List<String> allPatientID = model.getIDOfAllPatient();
+        for (String patient : allPatientID){
+            listModel.addElement(patient);
+        }
     }
 
     private void averageAgeButtonClicked(){
-        // do sth with this
-        // print this out to pop up???
-        model.getAverageAge();
+
+        String averageAge = model.getAverageAge();
+        JOptionPane.showMessageDialog(this, "Average Age of all patients is " + averageAge,
+                "Average Age", JOptionPane.INFORMATION_MESSAGE);
     }
 
     private void mostCommonYearButtonClicked(){
-        // do sth with this
-        // print this out to pop up???
+
         String commonYear = model.getMostCommonYearOfBirth();
+        JOptionPane.showMessageDialog(this, "The Most common year of birth of all patients is " + commonYear,
+                "Most common year of birth", JOptionPane.INFORMATION_MESSAGE);
     }
+
+    private void removeButtonClicked(){
+        int i = list.getSelectedIndex();
+        if (!(i < 0) || (i > listModel.size())) {
+            listModel.remove(i);
+        }
+        removeButton.setEnabled(false);
+    }
+
+    private void showSinglePatientButtonClicked(){
+        String selectedValue = list.getSelectedValue();
+
+        // This is the case where ID is clicked
+        if (selectedValue.contains("-")){
+            StringBuilder singlePatientJSONID =  model.getSinglePatientJSONbyID(selectedValue);
+            // write singlePatientJSONID in textArea
+            textArea.setText("");
+            textArea.append(String.valueOf(singlePatientJSONID));
+        }
+
+        // This is the case where name is clicked
+        else {
+            // separate first and last name
+            String name[] = selectedValue.split(" ");
+            final String firstName = name[0];
+            final String lastName = name[1];
+            StringBuilder singlePatientJSONName = model.getSinglePatientJSONbyName(firstName, lastName);
+            // write singlePatientJSONName in textArea
+            textArea.setText("");
+            textArea.append(String.valueOf(singlePatientJSONName));
+        }
+
+    }
+
 
     private void listClicked() {
         int n = list.getSelectedIndex();
         if (!(n < 0) || (n > listModel.size())) {
-            // show clicked single patient in json format in text area
-            //removeButton.setEnabled(true);
+            removeButton.setEnabled(true);
+            showSinglePatientButton.setEnabled(true);
         }
     }
 
 
-
     public static void main(String args[]){
-        SwingUtilities.invokeLater(GUI::new);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI();
+            }
+        });
     }
 
 }
