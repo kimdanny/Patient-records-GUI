ad3ef6c338559fc7bd68668c7bddf58372fe3f88
Danny Toeun Kim <34687639+kimdanny@users.noreply.github.com>
2019-04-02 14:45:58 +0100

    Update Model.java

--- src/Model.java
+++ src/Model.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.*;
-
+import java.time.*;
 /**
  * <Reference>
  * 1. private method 'mostCommonOne' is inspired by arshajii's answer.
@@ -12,8 +13,8 @@ import java.util.*;
 
 public class Model {
     private ReadCSV readCSV = new ReadCSV();
+    private ReadJSON readJSON = new ReadJSON();
     private List<Patient> patientList;
-    //private List<String> headerList;
 
     private JSONFormatter jsonFormatter;
     private StringBuilder allPatientsJSON;
@@ -23,11 +24,13 @@ public class Model {
     public List<Patient> readFile(String filepath){
         return readCSV.CSVtoList(filepath);
     }
+    public List<Patient> readJSON(String filepath) throws IOException {
+        return readJSON.JSONtoList(filepath);
+    }
 
     // Constructor that links file info to patientList and jsonFormatter
-    public Model(String filepath){
-        patientList = this.readFile(filepath);
-        //headerList = this.readCSV.getHeadersToList(filepath);
+    public Model(String filepath) throws Exception {
+        patientList = (filepath.endsWith(".csv")) ? this.readFile(filepath) : this.readJSON(filepath);
         jsonFormatter = new JSONFormatter(filepath);
         allPatientsJSON = jsonFormatter.allPatientsJSON();
     }
@@ -35,14 +38,17 @@ public class Model {
     /******************
      * Getter methods *
      ******************/
+    // all patient in json format
     protected StringBuilder getAllPatientJSON(){
         return allPatientsJSON;
     }
 
+    // single patient in json format searched by ID
     protected StringBuilder getSinglePatientJSONbyID(String MustBeID){
         return jsonFormatter.singlePatientJSONbyID(MustBeID);
     }
 
+    // single patient in json format searched by first and last name
     protected StringBuilder getSinglePatientJSONbyName(String first, String last){
         return jsonFormatter.singlePatientJSONbyName(first, last);
     }
@@ -63,27 +69,66 @@ public class Model {
         return idList;
     }
 
-    protected List<String> getDeadPatients(){
-        List<String> deadList = new ArrayList<>();
+    protected List<Patient> getDeadPatients(){
+        List<Patient> deadList = new ArrayList<>();
         for (Patient patient : patientList){
             if (patient.getDEATHDATE() != ""){
-                deadList.add(patient.getDEATHDATE());
+                deadList.add(patient);
             }
         }
         return deadList;
     }
 
     protected String getAverageAge(){
+        List<Integer> ageList = new ArrayList<>();
+        int sum = 0;
+        int averageAge;
 
-        List<String> ageList = new ArrayList<>();
+        //Getting current yy mm dd in int
+        String[] current = getCurrentDate().split("-");
+        int currentYear = Integer.parseInt(current[0]);
+        int currentMonth = Integer.parseInt(current[1]);
+        int currentDate = Integer.parseInt(current[2]);
 
+        LocalDate currentTobeSubtractFrom = LocalDate.of(currentYear, currentMonth, currentDate);
+
+        // Getting patient's yy mm dd in int
         for (Patient patient : patientList){
-            String birthDate = patient.getBIRTHDATE();
+            String getBirthDate = patient.getBIRTHDATE();
+            String[] birthDate = getBirthDate.split("-");
+            int year = Integer.parseInt(birthDate[0]);
+            int month = Integer.parseInt(birthDate[1]);
+            int day = Integer.parseInt(birthDate[2]);
 
+            LocalDate birthdate = LocalDate.of(year, month, day);
+
+            int age = getAge(birthdate, currentTobeSubtractFrom);
+
+            ageList.add(age);
         }
 
-        return "";
+        // calculating average age
+        for (int age : ageList){
+            sum = sum + age;
+        }
+        averageAge = sum / ageList.size();
+
+        return ""+averageAge;
     }
+
+    // helper method for getAverageAge
+    // return time difference in year as an int type
+    private int getAge(LocalDate birthDate, LocalDate currentDate) {
+        return Period.between(birthDate, currentDate).getYears();
+    }
+
+    // helper method for getAverageAge
+    // return current yyyy-mm--dd in String
+    private String getCurrentDate(){
+        return LocalDate.now().toString();
+    }
+
+
 
     protected String getMostCommonYearOfBirth(){
         List<String> yearsList = new ArrayList<>();
@@ -97,6 +142,7 @@ public class Model {
     }
 
     // helper class for getMostCommonYearOfBirth()
+    // Finds most common element in the list and returns the one in String
     private String mostCommonOne(List<String> inputList) {
 
         Map<String, Integer> map = new HashMap<>();
@@ -174,6 +220,74 @@ public class Model {
             }
         }
         return result;
+    }
+
+
+    public List<Patient> getOldest(){
+        List<Patient> oldestPatientList = new ArrayList<>();
+
+        int oldestAge = 0;
+
+        //Getting current yy mm dd in int
+        String[] current = getCurrentDate().split("-");
+        int currentYear = Integer.parseInt(current[0]);
+        int currentMonth = Integer.parseInt(current[1]);
+        int currentDate = Integer.parseInt(current[2]);
+
+        LocalDate currentYearMonthDate = LocalDate.of(currentYear, currentMonth, currentDate);
+
+        for (Patient patient : patientList){
+            // Getting patient's age
+            String getBirthDate = patient.getBIRTHDATE();
+            String[] birthDate = getBirthDate.split("-");
+            int year = Integer.parseInt(birthDate[0]);
+            int month = Integer.parseInt(birthDate[1]);
+            int day = Integer.parseInt(birthDate[2]);
+
+            LocalDate birthdate = LocalDate.of(year, month, day);
+
+            int patientAge = getAge(birthdate, currentYearMonthDate);
+
+            if (patientAge >= oldestAge){
+                oldestAge = patientAge;
+                oldestPatientList.add(patient);
+            }
+
+        }
+        return oldestPatientList;
+    }
+
+    public List<Patient> AgeRange0to19() {
+
+        List<Patient> result = new ArrayList<>();
+
+        //Getting current yy mm dd in int
+        String[] current = getCurrentDate().split("-");
+        final int currentYear = Integer.parseInt(current[0]);
+        final int currentMonth = Integer.parseInt(current[1]);
+        final int currentDate = Integer.parseInt(current[2]);
+
+        LocalDate currentYearMonthDate = LocalDate.of(currentYear, currentMonth, currentDate);
+
+        for (Patient patient : patientList) {
+            // Getting patient's age
+            String getBirthDate = patient.getBIRTHDATE();
+            String[] birthDate = getBirthDate.split("-");
+            int year = Integer.parseInt(birthDate[0]);
+            int month = Integer.parseInt(birthDate[1]);
+            int day = Integer.parseInt(birthDate[2]);
+
+            LocalDate birthdate = LocalDate.of(year, month, day);
+
+            int patientAge = getAge(birthdate, currentYearMonthDate);
+
+            // check age range
+            if (0 <= patientAge && patientAge <= 19) {
+                result.add(patient);
+            }
+        }
+        return result;
+
     }
 
 }
